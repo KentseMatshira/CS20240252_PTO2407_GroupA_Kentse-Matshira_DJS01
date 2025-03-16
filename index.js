135a11af2e0c7fb2ed9e92cc779a8a4f7924245b
KentseMatshira <kmatshira@yahoo.com>
2025-03-16 14:44:28 +0200

    debug parameters

--- index.js
+++ index.js
@@ -6,12 +6,12 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const velKilometersPerHour = 10000;// velocity (km/h)
+const accelMetersPerSecondSquared = 3;// acceleration (m/s^2)
+const timeSeconds = 3600;// seconds (1 hour)
+const initDistanceKilometers = 0;// distance (km)i
+const initFuelKilograms = 5000; // remaining fuel (kg)
+const fuelBurnRateKilogramsPerSecond = 0.5;// fuel burn rate (kg/s)
 
 
 const d2 = d + (vel*time) //calcultes new distance
